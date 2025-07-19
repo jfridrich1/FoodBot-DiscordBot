3b5ebab1a787dcec42082fd17f0700685d3429b5
jfridrich <jakub.fridrich1@gmail.com>
2025-07-19 19:41:27 +0200

    scrapping meal names

--- program.py
+++ program.py
@@ -1,13 +1,25 @@
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
 def scrapping():
     url = "https://eatandmeet.sk/"
     html_response = requests.get(url)
+    soup = BeautifulSoup(html_response.text, 'html.parser')
 
-    print(html_response)
+    today_menu_div = soup.find("div", class_="tab-pane fade  active  in")
+    menu_description = today_menu_div.find_all("div", class_="menu-description")
+    meals = []
 
-    #test
-    with open("test_scrap","w", encoding="utf-8") as f:
-        f.write(html_response.text)
-    return html_response.text
+    for desc_div in menu_description:
+        p_tag = desc_div.find("p", class_="desc")
+        if p_tag:
+            text = p_tag.find(text=True, recursive=False).strip()
+            meals.append(text)
+
+
+    with open("test_scrap.txt", "w", encoding="utf-8") as f:
+        for meal in meals:
+            f.write(meal + "\n")
+
+    return meals
