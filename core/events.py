6182456d372f4c4b2302d31073e68aadbd651208
jfridrich <jakub.fridrich1@gmail.com>
2025-07-23 00:06:32 +0200

    test fix
    6 55

--- core/events.py
+++ core/events.py
@@ -11,7 +11,7 @@ def use_events(bot):
 
         # Spustenie plánovača pri štarte bota
         scheduler = AsyncIOScheduler()
-        scheduler.add_job(daily_menu, CronTrigger(hour=0, minute=0), args=[bot])  # každý deň o polnoci
+        scheduler.add_job(daily_menu, CronTrigger(hour=6, minute=55), args=[bot])  # každý deň o polnoci
         #scheduler.add_job(daily_menu, CronTrigger(minute="*/30"), args=[bot])
         scheduler.start()
 
