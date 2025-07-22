1117e3b5a21bdf8b128894c6735e904e5ce60c17
jfridrich <jakub.fridrich1@gmail.com>
2025-07-22 14:57:28 +0200

    added daily schedule + JSON configuration possibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import threading
+import json
 from datetime import datetime
 from program import scrapping
 from discord.ext import commands
@@ -9,7 +10,11 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from exceptions import MenuNotFoundError, MenuBodyNotFoundError
 from emoji_mapping import title_emoji_mapper
 
-EMBED_COLOR = 0xffe28a
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+#EMBED_COLOR = 0xffe28a
+CONFIG_FILE = "config.json"
 load_dotenv()
 
 # Priradenie práv botovi
@@ -33,9 +38,23 @@ def start_http_server():
     print(f"Fake HTTP server running on port {port}.")
     server.serve_forever()
 
+if not os.path.exists(CONFIG_FILE):
+    with open(CONFIG_FILE, "w") as f:
+        json.dump({}, f)
+
+def load_config():
+    with open(CONFIG_FILE,"r") as f:
+        return json.load(f)
+
 @bot.event
 async def on_ready():
     print(f'Login {bot.user.name}')
+
+    # Spustenie plánovača pri štarte bota
+    scheduler = AsyncIOScheduler()
+    #scheduler.add_job(daily_menu, CronTrigger(hour=0, minute=0))  # každý deň o polnoci
+    scheduler.add_job(daily_menu, CronTrigger(second="*/10"))
+    scheduler.start()
 
 @bot.event
 async def on_message(message):
@@ -47,10 +66,12 @@ async def on_message(message):
         await message.channel.send('🇭🇺')
     await bot.process_commands(message)
 
+# Ping príkaz
 @bot.command()
 async def ping(ctx):
     await ctx.send('bu')
 
+# Príkaz na testovanie posielania obrázku
 @bot.command()
 async def testimage(ctx):
     url2 = "https://htmlcolorcodes.com/assets/images/colors/baby-blue-color-solid-background-1920x1080.png"
@@ -58,24 +79,46 @@ async def testimage(ctx):
     embed.set_image(url=url2)
     await ctx.send(embed=embed)
 
+# Príkaz na manuálne posielanie denného menu
 @bot.command()
 @commands.has_permissions(manage_messages=True)
-async def eats(ctx):
+async def eat(ctx):
+    # Premazanie správ pred poslaním novej spŕavy
+    await ctx.channel.purge(limit=10)
+    await send_daily_menu(ctx.channel, ctx.guild.id)
+
+async def daily_menu():
+    config = load_config()
+    for guild in bot.guilds:
+        guild_id = str(guild.id)
+        guild_config = config.get(guild_id)
+        if not guild_config:
+            continue
+
+        channel_id = guild_config.get("channel_id")
+        if not channel_id:
+            continue
+
+        channel = bot.get_channel(channel_id)
+        if channel:
+            await send_daily_menu(channel, guild.id)
+
+
+async def send_daily_menu(channel, guild_id):
     try:
         meal_names, main_prices, secondary_prices, allergens, meal_categories = scrapping()
-        embed_list = []
-
-        # Premazanie správ pred poslaním novej spŕavy
-        await ctx.channel.purge(limit=10)
+        config = load_config()
+        embed_color = config.get(str(guild_id), {}).get("embed_color", 0xffe28a)
 
         # Získanie dnešného dátumu
         current_date = datetime.today().strftime("%-d. %-m. %Y")
+        embed_list = []
 
         # Úvodná správa
         start_embed = discord.Embed(
             title=f"**Dnešné menu**",
             description=f"{current_date}",
-            color=EMBED_COLOR
+            color=embed_color
         )
         embed_list.append(start_embed)
 
@@ -85,21 +128,21 @@ async def eats(ctx):
             embed = discord.Embed(
                 title=f"{emoji} {meal_categories[i]} \n{meal_names[i]}",
                 description=f"Cena: *{main_prices[i]}*  **{secondary_prices[i]}**",
-                color=EMBED_COLOR
+                color=embed_color
             )
             embed.set_footer(text=f"{allergens[i]}")
             embed_list.append(embed)
 
         # Discord má limit 10 embedov v embede, ak ich je viac, treba poslať po dávkach po 10
         # Poslanie všetkých správ
-        await ctx.send(embeds=embed_list)
+        await channel.send(embeds=embed_list)
         
     except MenuNotFoundError as e:
-        await ctx.send("Nepodarilo sa nájsť dnešné menu.")
+        await channel.send("Nepodarilo sa nájsť dnešné menu.")
     except MenuBodyNotFoundError as e:
-        await ctx.send("Našlo sa menu, nepodarilo sa nájst položky z menu.")
+        await channel.send("Našlo sa menu, nepodarilo sa nájst položky z menu.")
     except Exception as e:
-        await ctx.send(f"Neočakávaná chyba: {type(e).__name__}: {e}")
+        await channel.send(f"Neočakávaná chyba: {type(e).__name__}: {e}")
 
 if __name__ == '__main__':
     threading.Thread(target=start_bot, daemon=True).start()
